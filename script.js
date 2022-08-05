2996a1e584f9ffe63ecec5221eb71b8c281733b7
Gabriel <gabrielbibiano1@hotmail.com>
2022-08-05 00:38:57 -0300

    Commit 1

---
+++ script.js
@@ -0,0 +1,24 @@
+function cursosgratuitosepagos(){
+    var gratuitosepagos = document.getElementById("gratuitosepagos");
+    gratuitosepagos.classList.toggle("ativo");
+}
+
+function cursosGratuitosCCertificadoGratuito(){
+    var gratuitocertificadogratuito = document.getElementById("gratuitocertificadogratuito");
+    gratuitocertificadogratuito.classList.toggle("ativo");
+}
+
+function cursosGratuitosCCertificadoPago(){
+    var gratuitocertificadopago = document.getElementById("gratuitocertificadopago");
+    gratuitocertificadopago.classList.toggle("ativo");
+}
+
+function cursosPagosCCertificado(){
+    var pagocomcertificado = document.getElementById("pagocomcertificado");
+    pagocomcertificado.classList.toggle("ativo");
+}
+
+function noticias(){
+    var noticias = document.getElementById("noticias");
+    noticias.classList.toggle("ativo");
+}
